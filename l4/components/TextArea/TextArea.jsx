0a192edf5f59a498ace9125da050a27d1a1fd0f7
dmitryi <elf_happy@mail.ru>
2021-08-04 15:25:59 +0300

    lesson4

---
+++ l4/components/TextArea/TextArea.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { useState} from 'react';
+import r from './TextArea.module.css'
+
+function TextArea(props) {
+    const [value, setValue] = useState('');
+
+    const targetValue = (event) => {
+        setValue(event.target.value);
+    }
+
+    const userMessage = () => {
+        let Mymessage = {
+                author: "I'm",
+                text: value,
+            };
+        setValue('');
+    }
+
+    const PressEntr = (event) => {
+        if(event.code === "Enter") {
+            userMessage();
+        };
+    }
+    
+
+    return (
+        <div className={r.center}>
+            <input ref={input => input && input.focus()} className={r.textarea} type="text" value={value} onChange={targetValue} onKeyDown={PressEntr}/>
+            <button className = {r.button} type="button" onClick={userMessage}>Отправить</button>
+        </div>
+    )
+}
+
+export default TextArea;
